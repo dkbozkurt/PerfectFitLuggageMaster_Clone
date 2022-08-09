d68f826aa8755e118fa4829f8f60423c5023b3d7
Doğukan Kaan Bozkurt <dkaanbozkurt@gmail.com>
2022-08-09 11:34:53 +0300

    PickUp and Drop Object

---
+++ Assets/Game/Scripts/Behaviours/SingletonBehaviour.cs
@@ -0,0 +1,46 @@
+﻿// Dogukan Kaan Bozkurt
+//      github.com/dkbozkurt
+
+using UnityEngine;
+
+namespace Game.Scripts.Behaviours
+{
+    public class SingletonBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T _instance = null;
+
+        public static T Instance
+        {
+            get
+            {
+                if (!_instance) // means _instance == null
+                {
+                    _instance = FindObjectOfType(typeof(T)) as T; //GameObject.FindObjectOfType<T>(); can be used too.
+                    if (!_instance)
+                    {
+                        // Debug.LogError("An instance of "+ typeof(T) +
+                        //                " is needed in the scene, but there is none.");
+
+                        _instance = new GameObject("Instance of " + typeof(T)).AddComponent<T>();
+                    }
+                }
+                
+                return _instance;
+            }
+            
+            set
+            {
+                _instance=value;
+            }
+        }
+        
+        private void Awake()
+        {
+            if(_instance != null)
+            {
+                Destroy(this.gameObject); // prevent duplicates
+            }
+        }
+        
+    }
+}
---
+++ Assets/Game/Scripts/Managers/PickUpDropManager.cs
@@ -0,0 +1,59 @@
+﻿// Dogukan Kaan Bozkurt
+//      github.com/dkbozkurt
+
+using System;
+using Game.Scripts.Behaviours;
+using UnityEngine;
+
+namespace Game.Scripts.Managers
+{
+    /// <summary>
+    /// Ref : https://www.youtube.com/watch?v=2IhzPTS4av4&ab_channel=CodeMonkey
+    /// </summary>
+    public class PickUpDropManager : SingletonBehaviour<PickUpDropManager>
+    {
+        [Header("Player Related")]
+        [SerializeField] private Transform playerCameraTransform;
+        [SerializeField] private Transform objectGrabPointTransform;
+        
+        [Space]
+        [Header("Grabbable Related")]
+        [SerializeField] private LayerMask pickUpLayerMask;
+        private GrabbableObject _grabbableObject;
+        private float _pickUpDistance = 100f;
+        
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if(_grabbableObject) return;
+                PickUpObject();
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                if(!_grabbableObject) return;
+                DropObject();
+            }
+
+        }
+
+        private void PickUpObject()
+        {
+            if (Physics.Raycast(playerCameraTransform.position, playerCameraTransform.forward,
+                    out RaycastHit raycastHit, _pickUpDistance, pickUpLayerMask))
+            {
+                if (raycastHit.transform.TryGetComponent(out _grabbableObject))
+                {
+                    _grabbableObject.Grab(objectGrabPointTransform);
+                }
+            }
+        }
+
+        private void DropObject()
+        {
+            _grabbableObject.Drop();
+            _grabbableObject = null;
+        }
+    }
+}
