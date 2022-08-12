b6e30b94c5df41231a4ec1711415f754626d9c8d
Doğukan Kaan Bozkurt <dkaanbozkurt@gmail.com>
2022-08-12 12:02:43 +0300

    Luggage mats and Random mat setter

---
+++ Assets/Game/Scripts/Behaviours/RandomMaterialSetter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class RandomMaterialSetter : MonoBehaviour
+{
+    [SerializeField] private List<Material> assignableMats;
+    [SerializeField] private int materialIndexToChange;
+
+    private void Awake()
+    {
+        SetRandomMaterial();   
+    }
+
+    private void SetRandomMaterial()
+    {
+        transform.GetChild(0).GetComponent<MeshRenderer>().materials[materialIndexToChange].color = assignableMats[GetRandomNumber()].color;
+    }
+
+    private int GetRandomNumber()
+    {
+        return Random.Range(0, assignableMats.Count);
+    }
+}
